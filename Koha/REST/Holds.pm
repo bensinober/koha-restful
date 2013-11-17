7ccba5eafe379d9deeb7d03498d369f67bf058f4
Benjamin Rokseth <bensinober@gmail.com>
2013-11-17 23:30:44 +0100

    holds

--- Koha/REST/Holds.pm
+++ Koha/REST/Holds.pm
@@ -5,14 +5,17 @@ use Modern::Perl;
 
 use Koha::REST::Response qw(format_response response_boolean);
 use C4::Reserves;
+use C4::HoldsQueue;
 use C4::Circulation;
 use C4::Biblio;
+
 use C4::Items;
 use C4::Branch;
 use C4::Members;
 use YAML;
 use File::Basename;
 use JSON;
+use Data::Dumper qw(Dumper);
 
 sub setup {
     my $self = shift;
@@ -28,38 +31,48 @@ sub rm_get_holds_for_branch {
     return format_response($self, get_holds_for_branch($branchcode));
 }
 
+# return array of biblio items with pendings holds
+sub get_pending_holds {
+    my @pending_holds = C4::HoldsQueue::GetBibsWithPendingHoldRequests();
+    return @pending_holds;
+}
+
 # return current holds for a branch
 sub get_holds_for_branch {
     my ($branchcode) = @_;
     return [] unless ($branchcode);
 
     my $response = [];
-    my @holds = C4::Reserves::GetReservesForBranch($branchcode);
-    foreach my $hold (@holds) {
-        # Up to Koha 3.8 GetBiblio returns an array whose last element is the
-        # biblio hash.
-        # Starting with Koha 3.10 GetBiblio returns only the biblio hash.
-        # Getting the last element of what is returned by this sub allow to be
-        # compatible with all versions.
-        my $biblio = (C4::Biblio::GetBiblio($hold->{biblionumber}))[-1];
-        my $item = C4::Items::GetItem($hold->{itemnumber});
-        push @$response, {
-            hold_id => $hold->{reserve_id},
-            rank => $hold->{priority},
-            reservedate => $hold->{reservedate},
-            biblionumber => $hold->{biblionumber},
-            branchcode => $hold->{branchcode},
-            itemnumber => $hold->{itemnumber},
-            title => $biblio ? $biblio->{title} : '',
-            barcode => $item ? $item->{barcode} : '',
-            itemcallnumber => $item ? $item->{itemcallnumber} : '',
-            branchname => C4::Branch::GetBranchName($hold->{branchcode}),
-            cancellationdate => $hold->{cancellationdate},
-            found => $hold->{found},
-        };
-    }
+    
+    #my @holds = C4::Reserves::GetReservesForBranch($branchcode);
+    my @pending_holds = get_pending_holds();
+    return $pending_holds;
+    # foreach my $pending_hold (@pending_holds) {
+    #     my @hold = C4::Reserves::GetReservesFromBiblionumber($pending_hold);
+    #     push @$response, {
+    #         hold => @hold[0];
+    #     };
+    # }
+    # foreach my $hold (@holds) {
+    #     my $biblio = (C4::Biblio::GetBiblio($hold->{biblionumber}))[-1];
+    #     my $item = C4::Items::GetItem($hold->{itemnumber});
+    #     push @$response, {
+    #         hold_id => $hold->{reserve_id},
+    #         rank => $hold->{priority},
+    #         reservedate => $hold->{reservedate},
+    #         biblionumber => $hold->{biblionumber},
+    #         branchcode => $hold->{branchcode},
+    #         itemnumber => $hold->{itemnumber},
+    #         title => $biblio ? $biblio->{title} : '',
+    #         barcode => $item ? $item->{barcode} : '',
+    #         itemcallnumber => $item ? $item->{itemcallnumber} : '',
+    #         branchname => C4::Branch::GetBranchName($hold->{branchcode}),
+    #         cancellationdate => $hold->{cancellationdate},
+    #         found => $hold->{found},
+    #     };
+    # }
 
-    return $response;
+    # return $response;
 }
 
 1;
